738a5a796b2e6d65560022c61468281f2bd46042
Ronald <ronystivens@hotmail.com>
2022-05-04 23:42:47 +0200

    inputs on scripts

---
+++ thirdscript-inputs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter your skill:"
+read SKILL
+
+echo "Your $SKILL skill is in high demand in the IT Industry"
+
+read -p "Username:" USER
+read -sp "Password:" pass
+
+echo
+echo "Login Successfull: Welcome User $USER"
